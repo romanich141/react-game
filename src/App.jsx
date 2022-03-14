a9abb993c8a55d9f2e73ee4348680375f97da7f5
roma_ovcharenko <roma_ovcharenko@ukr.net>
2022-03-14 23:27:10 +0200

    changes

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+// import { useReducer } from "react";
+// import Context from "./context/Context";
+// import { defaultStore } from "./store/store";
+// import { reducer } from "./store/reducer";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import { Game, Welcome, FinishGame, NotFound } from "./components";
+// import "./styles/main.scss";
+
+const App = () => '111';
+
+export default App;
