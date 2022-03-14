76d08d56132ef0710d5718eb54e4b900770f02ad
roma_ovcharenko <roma_ovcharenko@ukr.net>
2022-03-14 23:36:57 +0200

    add component not found page

---
+++ src/components/pages/NotFound.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function NotFound() {
+  return (
+    <div className="not-found">
+      <div className="info-text">Page Not Found</div>
+    </div>
+  );
+}
+
+export default NotFound;
