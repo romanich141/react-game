8dd0ae0bf980d08a280dc1b7945f23d68f7ebef0
roma_ovcharenko <roma_ovcharenko@ukr.net>
2022-03-14 23:26:56 +0200

    add helpers

--- src/helpers.js
+++ src/helpers.js
@@ -1,16 +1,16 @@
-export const arrayEquals = (a, b) => {
-  return (
-    Array.isArray(a) && Array.isArray(b) && a.every((val) => b.includes(val))
-  );
-};
+export const arrayEquals = (a, b) => (
+  Array.isArray(a) && Array.isArray(b) && a.every((val) => b.includes(val))
+);
 
-export const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+export const delay = (ms) => new Promise((resolve) => {
+  setTimeout(resolve, ms)
+});
 
-export const addCurrrency = (value, currency = "$") => `${currency} ${value}`;
+export const addCurrrency = (value, currency = '$') => `${currency} ${value}`;
 
 export const numeric = {
-  0: "A",
-  1: "B",
-  2: "C",
-  3: "D"
+  0: 'A',
+  1: 'B',
+  2: 'C',
+  3: 'D',
 };
