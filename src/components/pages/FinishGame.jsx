59e60cf8b28d58f8fae57cde007ea5bf36594aee
roma_ovcharenko <roma_ovcharenko@ukr.net>
2022-03-14 23:37:17 +0200

    add component finish game

---
+++ src/components/pages/FinishGame.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { actionTypeReset } from '../../store/actions';
+import { ReactComponent as Hand } from '../../assets/images/hand.svg';
+import { addCurrrency } from '../../helpers';
+import useStore from '../../custom-hooks/useStore';
+
+function FinishGame() {
+  const { dispatch, store } = useStore();
+
+  return (
+    <div className="finish">
+      <Hand className="logo" />
+      <div>
+        <div className="finish__title">Total score:</div>
+        <div className="title">{`${addCurrrency(store.score)} earned`}</div>
+        <Link
+          className="btn btn--orange"
+          to="/"
+          onClick={() => {
+            dispatch({ type: actionTypeReset });
+          }}
+        >
+          Try again
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default FinishGame;
